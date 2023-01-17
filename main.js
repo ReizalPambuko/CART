3a84b7dc71bd91cf637f12f2659937de166dc8eb
rizalpambuko700@gmail.com <rizalpambuko700@gmail.com>
2023-01-17 08:55:07 +0700

    FILTER SHOP

---
+++ main.js
@@ -0,0 +1,120 @@
+const product = {
+  data: [
+   {
+     "namaProduct": "Celana Cewe Pink",
+     "category": "celana",
+     "price": "100.000",
+     "image": "./image/11.jpg",
+   },
+  {
+     "namaProduct": "Baju Santai",
+     "category": "baju",
+     "price": "150.000",
+     "image": "./image/12.jpg",
+    },
+  {
+    "namaProduct": "Baju polos putih",
+    "category": "baju",
+    "price": "100.000",
+    "image": "./image/13.jpg",
+  },
+  {
+    "namaProduct": "Celana cowo",
+    "category": "celana",
+    "price": "190.000",
+    "image": "./image/14.jpg",
+  },
+  {
+    "namaProduct": "Jam Rolex",
+    "category": "jam",
+    "price": "10.000.000",
+    "image": "./image/15.jpg",
+  },
+  {
+    "namaProduct": "Jaket Coklat",
+    "category": "jaket",
+    "price": "300.000",
+    "image": "./image/16.jpg",
+  },
+ ]}
+
+for(let i of product.data){
+  const card = document.createElement("div");
+  card.classList.add("card",i.category,"hide");
+  
+  const container = document.createElement("div");
+  
+  container.classList.add("img-container");
+  
+  const imgContainer = document.createElement("img");
+  imgContainer.setAttribute("src",i.image);
+  
+  
+ container.appendChild(imgContainer);
+ card.appendChild(container);
+  
+  const productTeks = document.createElement("h5");
+  productTeks.classList.add("teks-five")
+  productTeks.innerHTML = i.namaProduct.toUpperCase();
+  container.appendChild(productTeks);
+  const harga = document.createElement("h6");
+  
+  harga.innerHTML = "Rp " + i.price;
+  container.appendChild(harga)
+  
+  
+  
+  document.getElementById("product").appendChild(card);
+  
+}
+
+
+function productt(value) {
+  const valueBtn = document.querySelectorAll(".value-btn");
+  valueBtn.forEach(va => {
+    if(value.toUpperCase() == va.innerHTML.toUpperCase()){
+       va.classList.add("active")
+    }
+    else{
+      va.classList.remove("active")
+    }
+  })
+  
+  
+  let cards = document.querySelectorAll(".card");
+  
+  cards.forEach(cr => {
+    if (value == 'semua'){
+      cr.classList.remove('hide')
+    }
+    else{
+      if(cr.classList.contains(value)){
+        cr.classList.remove('hide')
+      }else{
+        cr.classList.add('hide')
+      }
+    }
+  })
+}
+
+
+document.querySelector("#value-btn").addEventListener("click",function(){
+  let inputV = document.querySelector("#inputV").value;
+  let card = document.querySelectorAll(".card");
+  let teksFive = document.querySelectorAll(".teks-five");
+  
+  
+  teksFive.forEach((teks,index) => {
+    if(teks.innerText.includes(inputV.toUpperCase())){
+      card[index].classList.remove("hide")
+    }
+    else{
+      card[index].classList.add("hide")
+    }
+  })
+})
+
+
+window.onload = () => {
+  productt("semua");
+}
